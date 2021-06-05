d82609d565e5b0d943c548f13f042f3982eee15c
atharva_dawande <atharva.dawande603@gmail.com>
2021-06-06 02:42:21 +0530

    final

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ var ctx5 = canvas5.getContext('2d');
 
 var posx=0;
 setInterval(function(){
-posx+=1;
+posx+=2;
     ctx5.fillStyle=" rgba(2, 2, 2, 0.068)";
     ctx5.fillRect(0,0,canvas5.width,canvas5.height);
 
@@ -23,19 +23,19 @@ var canvas2 = document.getElementById('canvas2');
 var ctx2 = canvas2.getContext('2d');
 
 
-var posx2=-50;
+var posx2=-105;
 setInterval(function(){
 posx2+=1;
     ctx2.fillStyle="black";
     ctx2.fillRect(0,0,canvas2.width,canvas2.height);
 
-    ctx2.fillStyle="white";
+    ctx2.fillStyle=" rgba(0, 0, 255, 0.374)";
 
     ctx2.beginPath();
     ctx2.fillRect(posx2,posx2,105,105);
     ctx2.fill();
    if(posx2>canvas2.height +20 ){
-       posx2=-50;
+       posx2=-105;
    } 
   
 
